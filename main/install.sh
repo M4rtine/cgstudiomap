c5beb2a6ed7a6396ad0bc80ac3864720f82fe1c4
Jordi Riera <jordi.riera@savoirfairelinux.com>
2015-04-21 13:28:01 -0400

    [IMP] Add python depenencies.

--- main/install.sh
+++ main/install.sh
@@ -4,6 +4,7 @@ PROJECT_HOME=`pwd`
 # Force to use the frozen configuration.
 # BUILDOUT_CFG=$PROJECT_HOME/frozen.cfg
 BUILDOUT_CFG=$PROJECT_HOME/frozen.cfg
+#PARTS="reset_repos odoo"
 PARTS="odoo"
 
 # Prepare buildout environment
