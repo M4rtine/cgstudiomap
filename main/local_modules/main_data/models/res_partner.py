863f736aad22e22f94673429357c312f3a74ad42
Jordi Riera <kender.jr@gmail.com>
2015-05-20 16:12:54 -0400

    [#83][IMP] Set open and closed states for res.partner.

--- main/local_modules/main_data/models/res_partner.py
+++ main/local_modules/main_data/models/res_partner.py
@@ -5,7 +5,7 @@
 # This module copyright (C)  Jordi Riera <kender.jr@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
+# it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
@@ -18,12 +18,15 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import logging
 from openerp import api, fields
 from openerp.addons.base_geolocalize.models.res_partner import (
     geo_find, geo_query_address
 )
 
 from openerp.addons.base_geoengine import geo_model
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
 
 
 class ResPartner(geo_model.GeoModel):
@@ -36,7 +39,6 @@ class ResPartner(geo_model.GeoModel):
                 street=vals.get('street'),
                 zip=vals.get('zip'),
                 city=vals.get('city'),
-                # state=vals.get('state'),
                 country=self.env['res.country'].browse(
                     vals.get('country_id')).name,
             )
@@ -57,8 +59,31 @@ class ResPartner(geo_model.GeoModel):
         return super(ResPartner, self).write(
             self.add_geo_localization_details(vals))
 
+    def _states(self):
+        return [('open', 'Open'), ('closed', 'Closed for business')]
+
+    @api.multi
+    def action_close_company(self):
+        """Change the state of the companies to set them as closed."""
+        self.state = 'closed'
+
+    @api.multi
+    def action_open_company(self):
+        """Change the state of the companies to set them as opened."""
+        self.state = 'open'
+
     country_id = fields.Many2one(required=True)
     website = fields.Char(required=True)
     street = fields.Char(required=True)
     city = fields.Char(required=True)
     zip = fields.Char(required=True)
+
+    state = fields.Selection(
+        selection='_states',
+        default='open',
+        string='Status',
+        readonly=True,
+        copy=False,
+        help='Current state of the company.',
+        select=True
+    )
