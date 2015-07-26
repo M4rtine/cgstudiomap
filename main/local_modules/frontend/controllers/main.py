db1625d1ee06c571197bfa5cdf314743f816bec5
Jordi Riera <kender.jr@gmail.com>
2015-07-26 11:08:27 -0400

    [#212][IMP] Cache of the heavy request on the home page.

--- main/local_modules/frontend/controllers/main.py
+++ main/local_modules/frontend/controllers/main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
+import collections
+import functools
 import random
 
 import time
@@ -8,7 +10,39 @@ import logging
 from openerp.addons.web import http
 from openerp.http import request
 from openerp.addons.website.controllers.main import Website
+
 _logger = logging.getLogger(__name__)
+
+
+class Memoized(object):
+    """Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    """
+
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable. a list, for instance.
+            # better to not cache than blow up.
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+    def __repr__(self):
+        """Return the function's docstring."""
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        """Support instance methods."""
+        return functools.partial(self.__call__, obj)
 
 
 class MainPage(Website):
@@ -19,23 +53,20 @@ class MainPage(Website):
         page = 'homepage'
         env = request.env
         user_pool = env['res.users']
-        partner_pool = env['res.partner']
+        # optimisation as it is used in get_partners_by_country
+        self.partner_pool = env['res.partner']
         country_pool = env['res.country']
         ammap_homepage = env.ref('frontend.homepage_ammap_config')
         filters = [('active', '=', True), ('is_company', '=', True)]
+
         by_countries = defaultdict(int)
         for country in country_pool.search([]):
-            number_partners = partner_pool.search_count(
-                filters + [('country_id', '=', country.id)],
-            )
-            if number_partners:
-                by_countries[country] = number_partners
+            by_countries.update(self.get_partners_by_country(country))
+
 
         # https://github.com/cgstudiomap/cgstudiomap/issues/177
-        partners = [
-            p for p in partner_pool.search(filters + [('image', '!=', False)])
-        ]
-
+        # search return a recordset and we cannot do len() on it.
+        partners = [p for p in self.partner_pool.search(filters)]
         sample_partners = random.sample(partners, min(len(partners), 8))
 
         values = {
@@ -43,12 +74,29 @@ class MainPage(Website):
             'geochart_data': by_countries,
             'geochart_target': 'geochart_div',
             'ammap_config': ammap_homepage,
-            'nbr_partners': partner_pool.search_count(filters),
+            'nbr_partners': self.partner_pool.search_count(filters),
             'nbr_countries': len(by_countries.keys()),
             'nbr_users': user_pool.search_count([('active', '=', True)]),
             'partners': sample_partners,
         }
 
         time2 = time.time()
-        _logger.debug('function took %0.3f ms' % ((time2-time1)*1000.0))
+        _logger.debug('function took %0.3f ms' % ((time2 - time1) * 1000.0))
         return request.render('frontend.homepage', values)
+
+    @Memoized
+    def get_partners_by_country(self, country):
+        """Method to be memorized that return the number of partner in a country.
+
+        :param instance country: record of a country
+        :return: dict {country instance: count of partners in the country}
+        """
+        filters = [('active', '=', True), ('is_company', '=', True)]
+        number_partners = self.partner_pool.search_count(
+            filters + [('country_id', '=', country.id)],
+        )
+        if number_partners:
+            return {country: number_partners}
+
+        return {}
+
