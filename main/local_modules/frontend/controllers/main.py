9b989ce46a0761a395a623a109f6b60cb1995b28
Jordi Riera <kender.jr@gmail.com>
2015-11-11 20:11:35 -0500

    [#294][IMP]frontend.main: Enforce cgstudiomap is not displayed in the most popular section.

--- main/local_modules/frontend/controllers/main.py
+++ main/local_modules/frontend/controllers/main.py
@@ -3,13 +3,13 @@ from collections import defaultdict
 import collections
 import functools
 import random
-
 import time
 import logging
 
 from openerp.addons.web import http
 from openerp.http import request
 from openerp.addons.website.controllers.main import Website
+
 
 _logger = logging.getLogger(__name__)
 
@@ -58,7 +58,7 @@ class MainPage(Website):
             """
             number_partners = self.partner_pool.search_count(
                 filters + [('country_id', '=', country.id)],
-                )
+            )
             if number_partners:
                 return {country: number_partners}
 
@@ -72,20 +72,13 @@ class MainPage(Website):
         # optimisation as it is used in get_partners_by_country
         self.partner_pool = env['res.partner']
         country_pool = env['res.country']
-        filters = [('active', '=', True), ('is_company', '=', True)]
-
+        filters = [
+            ('active', '=', True),
+            ('is_company', '=', True),
+        ]
         by_countries = defaultdict(int)
         for country in country_pool.search([]):
             by_countries.update(get_partners_by_country(country))
-
-        # https://github.com/cgstudiomap/cgstudiomap/issues/177
-        # search return a recordset and we cannot do len() on it.
-        partners = [
-            p for p in self.partner_pool.search(
-                filters + [('image', '!=', False)]
-            )
-        ]
-        sample_partners = random.sample(partners, min(len(partners), 8))
 
         values = {
             'page': page,
@@ -94,9 +87,53 @@ class MainPage(Website):
             'nbr_partners': self.partner_pool.search_count(filters),
             'nbr_countries': len(by_countries.keys()),
             'nbr_users': user_pool.search_count([('active', '=', True)]),
-            'partners': sample_partners,
+            'partners': self.get_most_popular_studios(8),
         }
 
         time2 = time.time()
         _logger.debug('function took %0.3f ms' % ((time2 - time1) * 1000.0))
         return request.render('frontend.homepage', values)
+
+
+    @staticmethod
+    def get_most_popular_studios(sample):
+        """Return a list of partners that have a logo.
+
+        The list is filtered to just returns partner that match:
+        - is active
+        - is a company
+        - is not the partner related to cgstudiomap
+        - has an image.
+        :param partner_pool: pool of partners
+        :param list filters: domains to apply on top of the domain about images.
+        :return: list of partner records.
+        """
+        env = request.env
+        company_pool = env['res.company']
+        partner_pool = env['res.partner']
+        # #294
+        # Looking for cgstudiomap to avoid to have it displayed.
+        # cgstudiomap is actually the partner linked to the res.company
+        # of the instance.
+        # looking for the first (and only so far) res.company
+        company = company_pool.browse(1)
+
+        # https://github.com/cgstudiomap/cgstudiomap/issues/177
+        # search return a recordset and we cannot do len() on it.
+        partners = [
+            partner for partner in partner_pool.search(
+                [
+                    ('active', '=', True),
+                    ('is_company', '=', True),
+                    ('id', '!=', company.partner_id.id),
+                    [('image', '!=', False)]
+                ]
+            )
+        ]
+
+        # doing kind of unittest in here as I do not know how to
+        # do unittest with request :(
+        assert company.partner_id.id not in partners, (
+            'cgstudiomap is in the most popular studio list'
+        )
+        return random.sample(partners, min(len(partners), sample))
