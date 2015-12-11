de2518710edf04aa2731774f28b004070dca39d5
David Mazeau <d.mazeau@gmail.com>
2015-12-11 01:03:31 +0100

    geochart options
    modification and responsive preparation

--- main/local_modules/frontend_homepage/static/js/geocharts.js
+++ main/local_modules/frontend_homepage/static/js/geocharts.js
@@ -6,13 +6,31 @@ function geo_chart(companies, target) {
     function drawRegionsMap() {
         var data = google.visualization.arrayToDataTable(companies);
         var options = {
+			legend: 'none',
             colorAxis: {colors: ['#E8CFF7', '#B86BE5']},
             backgroundColor: '#F2F2F2',
-            width: '100%'
+            keepAspectRatio: true,
+ 			width:100 + "%",
+ 			height:100 + '%',
+ 			tooltip: {textStyle: {color: '#444444'}, trigger:'focus'}
         };
         var chart = new google.visualization.GeoChart(
             document.getElementById(target)
         );
         chart.draw(data, options);
     }
+
+  go();
+
+  window.addEventListener('resize', go);
+
+  function go() {
+  	chart.draw(data, options);
+  }
 }
+
+
+
+
+
+ 
