92a68fde4e0e706cf37336c71c887cf54fad5386
David Mazeau <d.mazeau@gmail.com>
2015-12-16 15:58:24 +0100

    #360-add bootstrap-table
    HTML mode

---
+++ main/local_modules/frontend_listing/static/src/js/directory.js
@@ -0,0 +1,33 @@
+(function($){
+
+	
+	$(document).ready(function() {
+		
+		var $table = $('#table');
+			$(function () {
+		});
+
+	});
+
+})(jQuery);
+
+
+/*$('#table').bootstrapTable({
+    columns: [{
+        field: 'Logo',
+        title: 'Logo'
+    }, {
+        field: 'Name',
+        title: 'Name'
+    }, {
+        field: 'Email',
+        title: 'Email'
+    }],
+        field: 'Industries',
+        title: 'Industries'
+    }, {
+        field: 'Localtion',
+        title: 'Location'
+    }],
+
+});*/
