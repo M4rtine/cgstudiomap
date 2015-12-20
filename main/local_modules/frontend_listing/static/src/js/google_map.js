b5887f3ec3c640ccbd6d2e35eba04cc119af3ee0
Jordi Riera <kender.jr@gmail.com>
2015-12-20 18:50:14 -0500

    frontend_listing: implementation of snazzymap style in google map.

--- main/local_modules/frontend_listing/static/src/js/google_map.js
+++ main/local_modules/frontend_listing/static/src/js/google_map.js
@@ -12,24 +12,24 @@ function initialize(geoloc) {
     var mapOptions = {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(mapCanvas, mapOptions)
+    var map = new google.maps.Map(mapCanvas, mapOptions);
 
     var markers = [];
-    jQuery.each(geoloc, function(i, val) {
-        var contentString = '<div id="content">'+ i + '</div>';
+    jQuery.each(geoloc, function (i, val) {
+        var contentString = '<div id="content">' + i + '</div>';
 
         var infowindow = new google.maps.InfoWindow({
             content: contentString
-          });
+        });
 
         var marker = new google.maps.Marker({
             position: {lat: val[0], lng: val[1]},
             map: map,
             title: i
-          });
+        });
         //extend the bounds to include each marker's position
         bounds.extend(marker.position);
-        marker.addListener('click', function() {
+        marker.addListener('click', function () {
             infowindow.open(map, marker);
         });
         markers.push(marker);
@@ -39,5 +39,145 @@ function initialize(geoloc) {
     var mc = new MarkerClusterer(map, markers);
     //now fit the map to the newly inclusive bounds
     map.fitBounds(bounds);
+
+    //https://snazzymaps.com/style/13113/custom-applesque-theme
+    var styles_applesque = [
+        {
+            "featureType": "landscape.man_made",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f7f1df"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape.natural",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f7f1df"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape.natural.terrain",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.medical",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#fbd3da"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#bde6ab"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffe15f"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#efd151"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "black"
+                }
+            ]
+        },
+        {
+            "featureType": "transit.station.airport",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#cfb2db"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#a2daf2"
+                }
+            ]
+        }
+    ];
+    map.setOptions({styles: styles_applesque});
 }
 
