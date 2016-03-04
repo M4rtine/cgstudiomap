fe062040d4fd6e52b850174d9f50af44e263e044
Jordi Riera <kender.jr@gmail.com>
2016-03-03 21:03:09 -0500

    frontend_base: bring back res_partner.location needed by infowindows

--- main/local_modules/frontend_base/models/res_partner.py
+++ main/local_modules/frontend_base/models/res_partner.py
@@ -153,3 +153,16 @@ class ResPartner(models.Model):
         return title + body + footer
 
     small_image_url = fields.Char('Url to the small image of the partner.')
+
+    location = fields.Char('Location', compute='location_code')
+
+    @api.one
+    def location_code(self):
+        """Return the concatenation of city, state and country."""
+        elements = []
+        if self.city:
+            elements.append(self.city)
+        for element in (self.state_id, self.country_id):
+            if element:
+                elements.append(element.name)
+        self.location = ', '.join(elements)
