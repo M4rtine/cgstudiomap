87f5af0a28a8e12c923c1d41ff6fedd85d70cb5d
M4rtine <d.mazeau@gmail.com>
2016-06-02 20:53:55 +0200

    [#680][WIP]navbar-animation

--- main/local_modules/shared_web_theme/static/src/js/main.js
+++ main/local_modules/shared_web_theme/static/src/js/main.js
@@ -7,6 +7,23 @@
 		});
 	}
 	
+	
+	function checkScroll() {
+		var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+		if ($(window).scrollTop() > startY) {
+			$('.navbar').addClass("scrolled");
+		} else {
+			$('.navbar').removeClass("scrolled");
+		}
+	}
+
+	if ($('.navbar').length > 0) {
+		$(window).on("scroll load resize", function () {
+			checkScroll();
+		});
+	}
+	
 	$(".fill-box").fillBox();
 	
 	$(document).ready(function() {
