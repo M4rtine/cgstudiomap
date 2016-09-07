9e12ba3134d8c93f86bae8c4fb04f724d7ef8f40
Jordi Riera <kender.jr@gmail.com>
2016-09-06 20:31:34 -0400

    add res_partner_visit_count

---
+++ main/local_modules/res_partner_visit_count/res_partner.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution
+#    This module copyright (C)  cgstudiomap <cgstudiomap@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+
+from openerp import models, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    """Represent addition of visit_count field."""
+    _inherit = 'res.partner'
+    visit_count = fields.Integer(
+        'Number of visit for this partner',
+        readonly=True,
+        help='Number increased each time the frontend  page of the studio is opened.'
+    )
