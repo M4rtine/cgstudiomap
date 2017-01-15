31969a02f545b08f11adeef3073efcbccfdc4890
David Mazeau <d.mazeau@gmail.com>
2017-01-16 00:31:03 +0100

    780 map homepage (#787)
    * footer-signature-color
    
    * footer-socials-colors
    
    * footer-bg-listing
    
    * update-bootstrap-table
    
    * fix-social-dropshadow-colors
    
    * fix-sortable-new-method
    
    * hide-sortable-icon
    
    * overide-hide-sortable-icon
    
    * fix-cardviews-updates
    
    * WIP-align-center-pagination
    
    * pagination-always-center
    
    * #441-new-infowindows

--- main/local_modules/frontend_listing/static/src/js/google_map.js
+++ main/local_modules/frontend_listing/static/src/js/google_map.js
@@ -35,93 +35,11 @@ function initialize(geoloc) {
     //INFOWINDOWS
     var infowindow = new google.maps.InfoWindow({
         //content: contentString,
-        width: 300,
-        maxWidth: 350
+        width: 250,
+        maxWidth: 300
     });
 
-    // This event expects a click on a marker
-    // When this event is fired the Info Window is opened.
-    //google.maps.event.addListener(marker, 'click', function () {
-    //    infowindow.open(map, marker);
-    //});
 
-    // Event that closes the Info Window with a click on the map
-    google.maps.event.addListener(map, 'click', function () {
-        infowindow.close();
-    });
-
-    // *
-    // START INFOWINDOW CUSTOMIZE.
-    // The google.maps.event.addListener() event expects
-    // the creation of the infowindow HTML structure 'domready'
-    // and before the opening of the infowindow, defined styles are applied.
-    // *
-    google.maps.event.addListener(infowindow, 'domready', function () {
-        // Reference to the DIV that wraps the bottom of infowindow
-        var iwOuter = $('.gm-style-iw');
-
-        /* Since this div is in a position prior to .gm-div style-iw.
-         * We use jQuery and create a iwBackground variable,
-         * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
-         */
-        var iwBackground = iwOuter.prev();
-
-        // Removes background shadow DIV
-        iwBackground.children(':nth-child(2)').css({
-            'display': 'none'
-        });
-
-        // Removes white background DIV
-        iwBackground.children(':nth-child(4)').css({
-            'display': 'none'
-        });
-
-        // Moves the infowindow 115px to the right.
-        iwOuter.parent().parent().css({
-            left: '115px'
-        });
-
-        // Moves the shadow of the arrow 76px to the left margin.
-        iwBackground.children(':nth-child(1)').attr('style', function (i, s) {
-            return s + 'left: 76px !important;'
-        });
-
-        // Moves the arrow 76px to the left margin.
-        iwBackground.children(':nth-child(3)').attr('style', function (i, s) {
-            return s + 'left: 76px !important;'
-        });
-
-        // Changes the desired tail shadow color.
-        iwBackground.children(':nth-child(3)').find('div').children().css({
-            'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px',
-            'z-index': '1'
-        });
-
-        // Reference to the div that groups the close button elements.
-        var iwCloseBtn = iwOuter.next();
-
-        // Apply the desired effect to the close button
-        iwCloseBtn.css({
-            opacity: '1',
-            right: '50px',
-            top: '11px',
-            backgroundColor: '#b05ae2'
-        });
-
-        // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
-        if ($('.iw-content').height() < 140) {
-            $('.iw-bottom-gradient').css({
-                display: 'none'
-            });
-        }
-
-        // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
-        iwCloseBtn.mouseout(function () {
-            $(this).css({
-                opacity: '1'
-            });
-        });
-    });
 
     jQuery.each(geoloc, function (i, val) {
         var contentString = '<div id="content">' + val[2] + '</div>';
